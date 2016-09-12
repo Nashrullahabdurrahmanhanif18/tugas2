51e9bb54e4b59ccb3511e296543647e8e9644c94
Nashrullahabdurrahmanhanif18 <Nashrullahabdurrahmanhanif@yahoo.co.id>
2016-09-12 23:18:58 +0700

    Add files via upload

---
+++ tugas2.ino
@@ -0,0 +1,69 @@
+int ledPin[] ={8,9,10,11};
+int maxCount = 256;
+int delayInterval = 1000;
+
+void setup() 
+
+  {
+  for (int i=8 ; i<12 ; i++)
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
+    digitalWrite(A0, HIGH);
+    digitalWrite(A1, HIGH);
+    digitalWrite(A2, HIGH);
+
+  }
+
+void loop() 
+
+  {
+   
+    int counter =0;
+    while(counter < maxCount){
+    if(digitalRead(A0)==LOW)
+  {
+    counter++;
+    displayBinary(counter);
+    delay(delayInterval);   
+  }
+  
+  if(digitalRead(A1)==LOW)
+  
+  {
+    counter--;
+    displayBinaryReverse(counter);
+    delay(delayInterval);   
+  }
+  }
+}
+
+void displayBinary(byte numToShow)
+{
+  for (int i=0; i < 8 ;i++)
+  {
+  if (bitRead(numToShow, i)==1)
+  {
+    digitalWrite(ledPin[i], HIGH);
+  }
+    else
+    {
+      digitalWrite(ledPin[i], LOW);
+    }
+}
+}
+
+void displayBinaryReverse(byte numToShow)
+{
+  for (int i=8; i >=0 ;i--)
+  {
+  if (bitRead(numToShow, i)==1)
+  {
+    digitalWrite(ledPin[i], HIGH);
+  }
+    else
+    {
+      digitalWrite(ledPin[i], LOW);
+    }
+}
+}
